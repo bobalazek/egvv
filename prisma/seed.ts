6e0c3cba8d02495e220e54f261ea55c1cc2ded8e
Borut Balazek <bobalazek124@gmail.com>
2022-03-17 19:22:27 +0100

    WIP on the DB structure

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // TODO
+}
+
+main()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
