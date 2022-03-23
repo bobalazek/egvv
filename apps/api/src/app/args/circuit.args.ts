ef2435ac9df4e11dc7c76a4a92b5e44ad9e3abcc
Borut Balazek <bobalazek124@gmail.com>
2022-03-23 16:50:21 +0100

    Circuit args fix

--- apps/api/src/app/args/circuit.args.ts
+++ apps/api/src/app/args/circuit.args.ts
@@ -4,19 +4,4 @@ import { ArgsType, Field, ID } from '@nestjs/graphql';
 export class CircuitArgs {
   @Field(() => ID)
   id: number;
-
-  @Field()
-  slug: string;
-
-  @Field()
-  name: string;
-
-  @Field()
-  location: string;
-
-  @Field()
-  countryCode: string;
-
-  @Field()
-  url: string;
 }
