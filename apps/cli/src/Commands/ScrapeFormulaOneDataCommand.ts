ca91029bbc9b4048a787f610e22f95c554b22cbb
Borut Balazek <bobalazek124@gmail.com>
2022-03-23 18:48:46 +0100

    WIP

--- apps/cli/src/Commands/ScrapeFormulaOneDataCommand.ts
+++ apps/cli/src/Commands/ScrapeFormulaOneDataCommand.ts
@@ -1,13 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { Command } from 'commander';
+import puppeteer from 'puppeteer';
 
 export const addScrapeFormulaOneDataCommand = (program: Command) => {
   const command = program
     .command('scrape-formula-one-data')
     .action(async (options: any) => {
-      // TODO
-    });
+      const browser = await puppeteer.launch();
 
+      const page = await browser.newPage();
+      page.goto(
+        'https://www.formula1.com/en/racing/2020/Austria/Timetable.html'
+      );
+
+      await page.waitForSelector('.article-content');
+
+      // TODO
+
+      await browser.close();
+    });
   program.addCommand(command);
 };
