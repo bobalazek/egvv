cf7d02b0db9cf0b5463080774cf78495f8bb6acf
Borut Balazek <bobalazek124@gmail.com>
2022-03-29 18:35:44 +0200

    Cleanup stuff

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,16 +1,15 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to web!</title>
+        <title>EGGV</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
