bdfac709823328dafb4896ef68145d5aac9dc2fc
Borut Balazek <bobalazek124@gmail.com>
2022-04-01 16:43:43 +0200

    WIP on admin stuff

--- apps/admin-web/src/app/app.tsx
+++ apps/admin-web/src/app/app.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Admin, DataProvider } from 'react-admin';
+import buildGraphQLProvider from 'ra-data-graphql';
+
 import './app.module.css';
 
 export function App() {
-  return <>Admin App</>;
+  const [dataProvider, setDataProvider] = useState<DataProvider>();
+
+  useEffect(() => {
+    (async () => {
+      const newDataProvider = await buildGraphQLProvider({
+        // TODO
+      });
+      setDataProvider(newDataProvider);
+    })();
+  }, []);
+
+  if (!dataProvider) {
+    return <>Loading ...</>;
+  }
+
+  return <Admin dataProvider={dataProvider}></Admin>;
 }
 
 export default App;
