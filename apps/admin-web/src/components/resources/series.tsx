63ef1ae54c4c0e0120a8d01c21c8ba31f8d9b0b7
Borut Balazek <bobalazek124@gmail.com>
2022-04-06 17:50:44 +0200

    Add new season + delete a season from inside a series stuff

--- apps/admin-web/src/components/resources/series.tsx
+++ apps/admin-web/src/components/resources/series.tsx
@@ -3,7 +3,6 @@ import {
   Datagrid,
   TextField,
   Show,
-  SimpleShowLayout,
   ShowButton,
   Edit,
   SimpleForm,
@@ -17,6 +16,9 @@ import {
   ListProps,
   CreateProps,
   EditProps,
+  CreateButton,
+  TabbedShowLayout,
+  Tab,
 } from 'react-admin';
 import { SeasonList } from './season';
 
@@ -39,25 +41,44 @@ export const SeriesList = (props: ListProps) => (
   </List>
 );
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const SeriesShowTitle = (data: any) => <span>Series - {data.record.name}</span>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const SeriesShowAddSeasonButton = (data: any) => (
+  <CreateButton to={`/Season/create?seriesId=${data.record.id}`} size="large" variant="outlined" label="Add a season" />
+);
+
 export const SeriesShow = (props: ShowProps) => (
-  <Show {...props}>
-    <SimpleShowLayout>
-      <TextField source="slug" />
-      <TextField source="name" />
-      <TextField source="url" />
-      <ReferenceManyField
-        target="seriesId"
-        reference="Season"
-        label="Seasons"
-        sort={{
-          field: 'startAt',
-          order: 'desc',
-        }}
-        filter={{ seriesId: props.id }}
-      >
-        <SeasonList />
-      </ReferenceManyField>
-    </SimpleShowLayout>
+  <Show title={<SeriesShowTitle />} {...props}>
+    <TabbedShowLayout>
+      <Tab label="Summary">
+        <TextField source="slug" />
+        <TextField source="name" />
+        <TextField source="url" />
+      </Tab>
+      <Tab label="Seasons" path="seasons">
+        <ReferenceManyField
+          target="seriesId"
+          reference="Season"
+          label=""
+          sort={{
+            field: 'startAt',
+            order: 'desc',
+          }}
+          filter={{ seriesId: props.id }}
+        >
+          <SeasonList
+            title={' '}
+            options={{
+              deleteRedirect: `/series/${props.id}/show/seasons`,
+            }}
+          />
+        </ReferenceManyField>
+        <SeriesShowAddSeasonButton />
+        <br />
+      </Tab>
+    </TabbedShowLayout>
   </Show>
 );
 
