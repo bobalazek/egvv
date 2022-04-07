9190ddb2acd763cb84685c1a7591f44cb32d20e1
Borut Balazek <bobalazek124@gmail.com>
2022-04-07 09:16:15 +0200

    Move secret into shared constants

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
-import { JWT_SECRET } from '../constants';
+import { JWT_SECRET } from '@egvv/shared';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
--- libs/shared/src/Constants.ts
+++ libs/shared/src/Constants.ts
@@ -1,5 +1,6 @@
 export const DATABASE_URL = <string>process.env.DATABASE_URL;
 
 export const HTTP_SERVER_PORT = parseInt(<string>process.env.API_HTTP_SERVER_PORT || '8080');
-
 export const HTTP_SERVER_GRAPHQL_PATH = '/graphql';
+
+export const JWT_SECRET = <string>process.env.JWT_SECRET;
